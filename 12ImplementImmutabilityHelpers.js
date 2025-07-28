82f333b2025023037cc1077447aea9fb8eb8b71f
rohanpaul23 <paul.rohan540@gmail.com>
2025-07-29 00:14:06 +0530

    add comments

--- 12ImplementImmutabilityHelpers.js
+++ 12ImplementImmutabilityHelpers.js
@@ -1,60 +1,102 @@
 /**
- * @param {any} data
- * @param {Object} command
+ * Performs an immutable update on an object or array,
+ * according to a “command” specification.
+ *
+ * @param {Object|Array} data    The original data (object or array)
+ * @param {Object}        command A command object describing the updates
+ * @returns {Object|Array}        A new data structure with updates applied
  */
 function update(data, command) {
-    if (typeof data !== 'object' && !Array.isArray(data)) {
-      throw new Error();
-    }
-  
-    let copiedData =  Array.isArray(data) ? [...data] : {...data}
-    _update(copiedData, command);
-    return copiedData;
+  // Ensure we only operate on objects or arrays
+  if (typeof data !== 'object' && !Array.isArray(data)) {
+    throw new Error('update: first argument must be an object or array');
   }
-  
-  function _update(data, command) {
-    for (const key in command) {
-      if (key === '$push' && Array.isArray(command[key]) && Array.isArray(data)) {
-        data.push(...command[key]);
+
+  // Shallow‐copy the root so we don’t mutate the original
+  // - For arrays: create a new array with the same elements
+  // - For objects: create a new object with the same own properties
+  let copiedData = Array.isArray(data)
+    ? [...data]
+    : { ...data };
+
+  // Apply the nested updates
+  _update(copiedData, command);
+
+  // Return the new, updated copy
+  return copiedData;
+}
+
+/**
+ * Recursively walks the command object and applies updates
+ * to `data` in place (but `data` itself is already a shallow copy).
+ *
+ * Supported commands under each key:
+ *   - $push  : append items to an array
+ *   - $set   : replace a value
+ *   - $apply : transform a value via function
+ *   - $merge : shallow‐merge into an object
+ *
+ * You can also nest commands to update deep paths.
+ *
+ * @param {Object|Array} data    The (copied) data to mutate here
+ * @param {Object}        command The command spec for this level
+ */
+function _update(data, command) {
+  for (const key in command) {
+    // 1) $push: append elements into an array
+    if (
+      key === '$push' &&
+      Array.isArray(command.$push) &&
+      Array.isArray(data)
+    ) {
+      data.push(...command.$push);
+      return; // done with this command
+    }
+
+    // 2) $set: replace a property or array index
+    if (
+      typeof command[key] === 'object' &&
+      command[key].hasOwnProperty('$set')
+    ) {
+      data[key] = command[key].$set;
+      return; // stop further processing at this branch
+    }
+
+    // 3) $apply: apply a function to existing value (array‐specific here)
+    if (
+      typeof command[key] === 'object' &&
+      command[key].hasOwnProperty('$apply') &&
+      Array.isArray(data)
+    ) {
+      // Only apply if the index/key exists
+      if (data[key] !== undefined) {
+        data[key] = command[key].$apply(data[key]);
         return;
       }
-  
-      if (
-        typeof command[key] === 'object' &&
-        command[key].hasOwnProperty('$set')
-      ) {
-        data[key] = command[key].$set;
+    }
+
+    // 4) $merge: shallow‐merge an object into data[key]
+    if (
+      typeof command[key] === 'object' &&
+      command[key].hasOwnProperty('$merge')
+    ) {
+      // Only valid if target is already an object
+      if (typeof data[key] === 'object' && data[key] !== null) {
+        data[key] = {
+          ...data[key],
+          ...command[key].$merge,
+        };
         return;
-      }
-  
-      if (
-        typeof command[key] === 'object' &&
-        command[key].hasOwnProperty('$apply') &&
-        Array.isArray(data)
-      ) {
-        if (data[key]) {
-          data[key] = command[key].$apply(data[key]);
-          return;
-        }
-      }
-  
-      if (
-        typeof command[key] === 'object' &&
-        command[key].hasOwnProperty('$merge')
-      ) {
-        if (typeof data[key] === 'object') {
-          data[key] = {
-            ...data[key],
-            ...command[key].$merge,
-          };
-          return;
-        } else {
-          throw new Error();
-        }
-      }
-  
-      if (typeof command[key] === 'object') {
-        _update(data[key], command[key]);
+      } else {
+        throw new Error('update: cannot merge into non‐object');
       }
     }
+
+    // 5) Nested commands: descend into object property or array index
+    if (typeof command[key] === 'object') {
+      // e.g. command = { user: { stats: { $set: {...} } } }
+      _update(data[key], command[key]);
+      // no return here, in case there are multiple sibling keys
+    }
   }
+}
--- 16EventEmitter.js
+++ 16EventEmitter.js
@@ -1,30 +1,58 @@
+/**
+ * A simple EventEmitter implementation for subscribing to and emitting events.
+ */
 class EventEmitter {
+  /**
+   * Initialize a Map to hold eventName → array of callback functions.
+   */
   constructor() {
     this.callbacksMap = new Map();
   }
 
+  /**
+   * Subscribe to an event by name.
+   * @param {string} eventName - The name of the event to listen for.
+   * @param {Function} callback - The function to call when the event is emitted.
+   * @returns {{ release: Function }} - An object with a `release` method to unsubscribe.
+   */
   subscribe(eventName, callback) {
+    // If there are existing subscribers for this event, add to that list
     if (this.callbacksMap.has(eventName)) {
       let callBackFunctions = this.callbacksMap.get(eventName);
       callBackFunctions.push(callback);
       this.callbacksMap.set(eventName, callBackFunctions);
     } else {
+      // Otherwise, start a new list of subscribers for this event
       this.callbacksMap.set(eventName, [callback]);
     }
+
+    // Return an object that allows the subscriber to remove themselves
     return {
       release: () => {
         const allCallbacks = this.callbacksMap.get(eventName);
+        // Find the index of the callback to remove
         const callBackToRemove = allCallbacks.indexOf(callback);
-        allCallbacks.splice(callBackToRemove, 1);
+        if (callBackToRemove !== -1) {
+          // Remove the callback from the array
+          allCallbacks.splice(callBackToRemove, 1);
+        }
       },
     };
   }
 
+  /**
+   * Emit an event, invoking all subscribed callbacks with the provided arguments.
+   * @param {string} eventName - The name of the event to emit.
+   * @param {...any} args - Arguments to pass to each callback.
+   */
   emit(eventName, ...args) {
     const cbs = this.callbacksMap.get(eventName);
-    if (!cbs.length) {
+    // If no callbacks are registered, do nothing
+    if (!cbs || cbs.length === 0) {
       return;
     }
+
+    // Call each subscriber with the given arguments
     for (const cb of cbs) {
       cb.apply(this, args);
     }
